eaa1105a0d134771051486d6be0ea0d7a00df15c
plplplrrr <32988112+LyudmilaPetrashko@users.noreply.github.com>
2018-03-31 14:41:26 +0300

    Update 1.cpp

--- 03.03--chain data structures/1.cpp
+++ 03.03--chain data structures/1.cpp
@@ -1,46 +1,53 @@
 #include <iostream>
 using namespace std;
 
+
+//структура одного узла
 struct node{
 	double data;
 	node *next;
 };
 
+
+//структура для стека
 struct stack{
  	node *head;
- 	
- 	stack(){head=NULL;}
- 	
- 	
- 	// вывод стека
- 	void show(){
- 	    node *p=head;
- 	    if(p==NULL){
- 	        cout<<"stack empty";
- 	    }
- 	    else{
- 	        while(p!=NULL){
- 	            cout<<p->data<<" ";
- 	            p=p->next;
- 	        }
- 	    }
- 	    cout<<endl;
- 	}
+ 	int size;
+	
+	//конструктор
+ 	stack(){head=NULL; size=0;}
  	
  	
- 	//добавление элемента
- 	void push(double h){
- 	    node *q;
- 	    q= new node();
- 	    q->data=h;
- 	    if(head==NULL){
- 	        head=q;
- 	    }
- 	    else{
- 	        q->next=head;
- 	        head=q;
- 	    }
- 	}
+ // вывод стека
+ void show(){
+     node *p=head;
+     if(p==NULL){
+         cout<<"stack empty";
+     }
+     else{
+         while(p!=NULL){
+             cout<<p->data<<" ";
+             p=p->next;
+         }
+     }
+     cout<<endl;
+ }
+ 	
+ 	
+ //добавление элемента
+ void push(double h){
+     node *q;
+     q= new node();
+     q->data=h;
+     if(head==NULL){
+         head=q;
+     }
+     else{
+         q->next=head;
+         head=q;
+     }
+     size++;
+ }
  	
 
 //удаление элемента 	
@@ -55,8 +62,26 @@ double pop(){
         k=head->next;
         delete head;
         head=k;
+	size--;
     }
     return i;
+}
+	
+
+	
+//взятие значения первого элемента	
+double front(){
+    node *c=head;
+    if(head!=NULL){
+        for(int i=1; i<size; i++){
+        c=c->next;
+        }
+        return c->data;
+    }
+    else{
+        return 0;
+    
+}
 }
 
     
@@ -70,18 +95,33 @@ bool empty(){
  	      return 0;
  	  }
 }
- 	
-};
-int main(){
-	stack g;
-	g.push(9);
-	g.push(0);
-	g.push(7.65749);
-	g.push(67);
-	cout<<"pop: "<<g.pop()<<endl;
-	cout<<"show stack: ";
-	g.show();
-	cout<<"stack empty?: "<<g.empty()<<endl;
-	return 0;
+
+	
+	
+//деструктор
+~stack(){
+    node *p=head;
+    while(head!=NULL){  
+    p=head;
+    head=head->next ;
+    delete p;
+}
 }
 
+};
+
+
+
+int main(){
+	stack g;
+	g.push(9.8);
+	g.push(0.7);
+	g.push(7.65749);
+	g.push(67.1);
+	cout<<"pop: "<<g.pop()<<endl;
+	cout<<"front(if stack empty, write '0'): "<<g.front()<<endl;
+	cout<<"stack empty?: "<<g.empty()<<endl;
+	cout<<"show stack(left-back, right-front): ";
+	g.show();
+	return 0;
+}
