7cb05ebb0c396376203eb8ba4bb6bb285f2284b0
plplplrrr <32988112+LyudmilaPetrashko@users.noreply.github.com>
2018-04-15 23:42:46 +0300

    Update matrix.cpp

--- PROJECT/matrix.cpp
+++ PROJECT/matrix.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 
 class matrix{ //класс матриц
-   
+
 int n,m;  //n-кол-во строк,m-кол-во столбцов
 double** arr;
-    
+
 public:
-    
+
 matrix(int a, int b): n(a), m(b){ //конструктор
-    arr = new double*[n]; 
-    for (int i = 0; i < n; i++) 
+    arr = new double*[n];
+    for (int i = 0; i < n; i++)
         arr[i] = new double[m];
 }
 
@@ -20,33 +20,33 @@ matrix(){n=0; m=0; arr=0;} //конструктор по умолчанию
 
 
 void get(){  //заполнение матрицы
-    for (int i = 0; i < n; i++){     
-        for (int j = 0; j < m; j++) 
-        { 
-            cout << "Inter element " << "[" << i << "][" << j << "]:  "; 
-            cin >> arr[i][j]; 
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++)
+        {
+            cout << "Inter element " << "[" << i << "][" << j << "]:  ";
+            cin >> arr[i][j];
         }
     }
 }
-    
 
-    
+
+
 matrix(const matrix& matr){  //конструктор копирования
         n=matr.n;
         m=matr.m;
-        arr = new double*[matr.n]; 
-    for (int i = 0; i < matr.n; i++) 
+        arr = new double*[matr.n];
+    for (int i = 0; i < matr.n; i++)
         arr[i] = new double[matr.m];
 
-    for (int a = 0; a < matr.n; a++){ 
-        for (int b = 0; b < matr.m; b++) 
-        { 
+    for (int a = 0; a < matr.n; a++){
+        for (int b = 0; b < matr.m; b++)
+        {
          arr[a][b]=matr.arr[a][b];
         }
     }
     }
-    
-    
+
+
 matrix& operator=(const matrix& max){
          for(int i = n - 1; i>=0; i--){
                 delete [] arr[i];
@@ -54,27 +54,27 @@ matrix& operator=(const matrix& max){
          delete[] arr;
          n=max.n;
          m=max.m;
-          arr = new double*[max.n]; 
-    for (int i = 0; i < max.n; i++){ 
+          arr = new double*[max.n];
+    for (int i = 0; i < max.n; i++){
         arr[i] = new double[max.m];
     }
 
-    for (int x = 0; x < max.n; x++){ 
-        for (int y = 0; y < max.m; y++) 
-        { 
+    for (int x = 0; x < max.n; x++){
+        for (int y = 0; y < max.m; y++)
+        {
          arr[x][y]=max.arr[x][y];
         }
     }
     return *this;
     }
-    
-    
-    
+
+
+
 matrix operator+(const matrix& m1){
         matrix g(n,m), h;
         if(m==m1.m&&n==m1.n){
-        for (int x = 0; x < n; x++){ 
-            for (int y = 0; y < m; y++){ 
+        for (int x = 0; x < n; x++){
+            for (int y = 0; y < m; y++){
                 g.arr[x][y]=arr[x][y]+m1.arr[x][y];
             }
         }
@@ -84,14 +84,14 @@ matrix operator+(const matrix& m1){
             return h;
         }
     }
-    
-    
-        
+
+
+
 matrix operator-(const matrix& m2){
         matrix f(n,m), h;
         if(m==m2.m&&n==m2.n){
-        for (int x = 0; x < n; x++){ 
-            for (int y = 0; y < m; y++){ 
+        for (int x = 0; x < n; x++){
+            for (int y = 0; y < m; y++){
                 f.arr[x][y]=arr[x][y]-m2.arr[x][y];
             }
         }
@@ -101,9 +101,9 @@ matrix operator-(const matrix& m2){
             return h;
         }
      }
-     
-    
-    
+
+
+
 matrix operator*(const matrix& m3){
          matrix t(m,m3.n), h;
          if(m==m3.n){
@@ -125,14 +125,14 @@ matrix operator*(const matrix& m3){
 
 matrix operator*(double d){
     matrix w(n,m);
-    for (int x = 0; x < n; x++){ 
-        for (int y = 0; y < m; y++){ 
+    for (int x = 0; x < n; x++){
+        for (int y = 0; y < m; y++){
             w.arr[x][y]=arr[x][y]*d;
         }
     }
     return w;
 }
-        
+
 
 matrix transp ()  //транспонирование матрицы
 {
@@ -145,13 +145,13 @@ matrix transp ()  //транспонирование матрицы
 
 
 void print(){    //вывод матрицы
-    for (int i = 0; i < n; i++){ 
-        for (int j = 0; j < m; j++) { 
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++) {
           cout<<arr[i][j]<<" ";
         }
     cout<<endl;
     }
-}    
+}
 
 ~matrix(){  //деструктор
    for(int i = n - 1; i>=0; i--){
@@ -159,41 +159,43 @@ void print(){    //вывод матрицы
    }
    delete[] arr;
 }
-         
+
 };
 
 
 int main(){
-    matrix c(2,2), j(2,2), sum, dif, mult_m_m, mult_m_n, trans;
-    
+
+   matrix c(2,2), j(2,2), sum, dif, mult_m_m, mult_m_n, trans;
+
     c.get();
     cout<<endl<<"print c:"<<endl;
     c.print();
-    
+
+    cout<<endl;
+
     j.get();
     cout<<endl<<"print j:"<<endl;
     j.print();
-    
+
     sum=j+c;
     cout<<endl<<"print sum:"<<endl;
     sum.print();
-    
+
     dif=j-c;
-    cout<<endl<<"print dif:"<<endl;
+    cout<<endl<<"print difference:"<<endl;
     dif.print();
-    
-   
+
     mult_m_m=c*j;
-    cout<<endl<<"print mult_m_m:"<<endl;
+    cout<<endl<<"print product_m_m:"<<endl;
     mult_m_m.print();
-    
+
     mult_m_n=c*3;
-    cout<<endl<<"print mult_m_n:"<<endl;
+    cout<<endl<<"print product_m_n:"<<endl;
     mult_m_n.print();
-    
+
     trans=c.transp();
     cout<<endl<<"print transp:"<<endl;
     trans.print();
-    
+
     return 0;
 }
