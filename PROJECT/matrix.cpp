5e83e313a2890d1658273ab468740ac2fc094eb4
plplplrrr <32988112+LyudmilaPetrashko@users.noreply.github.com>
2018-04-24 23:12:23 +0300

    Update matrix.cpp

--- PROJECT/matrix.cpp
+++ PROJECT/matrix.cpp
@@ -16,6 +16,22 @@ matrix(int a, int b): n(a), m(b){ //конструктор
         arr[i] = new double[m];
 }
 
+matrix(double h, int k): n(k), m(k){ //конструктор для перевода double чисел в matrix для перемножения
+    arr=new double*[k];
+    for (int i=0; i<k; i++){
+        arr[i]=new double[k];
+    }
+    for(int j=0; j<k; j++){
+        for(int s=0; s<k; s++){
+        arr[j][s]=0;
+    }
+    }
+    for(int j=0; j<k; j++){
+        arr[j][j]=h;
+    }
+}
+
+
 matrix(){n=0; m=0; arr=0;} //конструктор по умолчанию
 
 
@@ -106,18 +122,21 @@ matrix operator-(const matrix& m2){
 
 matrix operator*(const matrix& m3){
          matrix t(m,m3.n), h;
-         if(m==m3.n){
-            for (int i = 0; i < m; ++ i){
-                for (int j = 0; j < m3.n; ++ j){
-                     for (int k = 0; k < m; ++ k){
-                        t.arr[i][j] += arr[i][k] * m3.arr[k][j];
-                     }
+         if(*this==matrix(*this)){
+            if(m==m3.n){
+                for (int i = 0; i < m; ++ i){
+                    for (int j = 0; j < m3.n; ++ j){
+                        for (int k = 0; k < m; ++ k){
+                            t.arr[i][j] += arr[i][k] * m3.arr[k][j];
+                        }
+                    }
                 }
             }
-         }
-         else{
-             return h;
-         }
+            else{
+                return h;
+            }
+            }
+
    return t;
 }
 
@@ -132,6 +151,18 @@ matrix operator*(double d){
     }
     return w;
 }
+
+bool operator==(const matrix& m4){
+    int k=0;
+    for(int i=0;i<n; i++){
+    for(int j=0; j<m; j++){
+    if(arr[i][j]==m4.arr[i][j])
+            k++;
+    }
+  }
+  if(k==n*m){return 1;}
+  else{return 0;}
+  }
 
 
 matrix transp ()  //транспонирование матрицы
@@ -165,12 +196,19 @@ void print(){    //вывод матрицы
 
 int main(){
 
-   matrix c(2,2), j(2,2), sum, dif, mult_m_m, mult_m_n, trans;
+    matrix c(2,2), j(2,2), sum, dif, mult_m_m, mult_m_n, multt, trans;
+    matrix t(7.0,2);
+    int boo;
+    cout<<"show double=>matrix"<<endl;
+    t.print();
 
     c.get();
     cout<<endl<<"print c:"<<endl;
     c.print();
 
+    cout<<"mult double*matrix"<<endl;
+    multt=t*c;
+    multt.print();
     cout<<endl;
 
     j.get();
@@ -196,6 +234,11 @@ int main(){
     trans=c.transp();
     cout<<endl<<"print transp:"<<endl;
     trans.print();
+
+    if(c==j){boo=1;}
+    else{boo=0;}
+    cout<<endl<<"print boo:"<<endl;
+    cout<<boo<<endl;
 
     return 0;
 }
