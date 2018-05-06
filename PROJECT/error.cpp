c0c88e67e0025d14dc548f113edbd3052c12a749
plplplrrr <32988112+LyudmilaPetrashko@users.noreply.github.com>
2018-05-07 01:31:11 +0300

    Update error.cpp

--- PROJECT/error.cpp
+++ PROJECT/error.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
+#include<algorithm>
 #include<cmath>
 #include<string>
 
 using namespace std;
+
 
 struct value{
     double av_val;//среднее значение
@@ -12,15 +14,15 @@ struct value{
     int a;//кол-во экспериментов
 
 
-    double read_er, scal_er, calib_er;//погрешности отсчитывания, по разбросу, градуировки
+    double dig_er, disp_er, calib_er;//погрешности отсчитывания, по разбросу, градуировки
 
     value(string n):  name(n) { //конструктор
             cout<<endl<<"enter the number of experiments("<<name<<"): ";
             cin>>a;
             double* arr=new double[a];
             double curr=0, cur=0;
-            cout<<endl<<"enter the reading error("<<name<<"): ";
-            cin>>read_er;
+            cout<<endl<<"enter the digitizing error("<<name<<"): ";
+            cin>>dig_er;
             cout<<"enter the calibration error("<<name<<"): ";
             cin>>calib_er;
             cout<<endl;
@@ -37,56 +39,45 @@ struct value{
             av_val=cur/a;
 
             for(int i=0; i<a; i++){
-                curr=curr+pow((av_val-arr[i]),2.0);// вычисление среднего квадратического отклонения
+                curr=curr+pow((av_val-arr[i]),2);// вычисление среднего квадратического отклонения
             }
             if(a==1){
-                scal_er=0;
+                disp_er=0;
             }
             else{
-            scal_er=2*sqrt(curr/(a-1));// c 80-90% точности?????
+            disp_er=2*sqrt(curr/(a-1));// c 90% точности
             }
-            error=sqrt(pow(read_er,2)+pow(scal_er,2)+pow(calib_er,2));
+            error=sqrt(pow(dig_er,2)+pow(disp_er,2)+pow(calib_er,2));
 
             delete[] arr;
     }
 
     value(){ //конструктор по умолчанию
-        name='0';
         av_val=0;
         error=0;
-        a=0;
-        read_er=0;
-        scal_er=0;
-        calib_er=0;
     }
 
 
     value(double x, double y, string h): av_val(x), error(y), name(h){ //конструктор для переменной, где все уже посчитано
-        a=0;
-        read_er=0;
-        scal_er=0;
-        calib_er=0;
         }
 
     value(const value& v){ //конструктор копирования
         av_val=v.av_val;
         error=v.error;
-        a=v.a;
-        read_er=v.read_er;
-        scal_er=v.scal_er;
-        calib_er=v.calib_er;
     }
 
     value& operator=(const value& v1){
         av_val=v1.av_val;
         error=v1.error;
-        a=v1.a;
-        read_er=v1.read_er;
-        scal_er=v1.scal_er;
-        calib_er=v1.calib_er;
         return *this;
     }
 
+
+     value& operator=(double vv1){
+        av_val=vv1;
+        error=0;
+        return *this;
+    }
 
     value operator+(const value& v2){
         value sum1;
@@ -129,7 +120,7 @@ struct value{
     value operator*(const value& v4){
         value mult2;
         mult2.av_val=av_val*v4.av_val;
-        mult2.error=mult2.av_val*sqrt(error*error/(av_val*av_val)+v4.error*v4.error/(v4.av_val*v4.av_val));
+        mult2.error=sqrt(error*error*v4.av_val*v4.av_val+v4.error*v4.error*av_val*av_val);
         return mult2;
     }
 
@@ -145,47 +136,74 @@ struct value{
     value operator/(const value& v5){
         value qu2;
         qu2.av_val=av_val/v5.av_val;
-        qu2.error=qu2.av_val*sqrt(pow(error, 2.0)/pow(av_val, 2.0)+pow(v5.error, 2.0)/pow(v5.av_val, 2.0));
+        qu2.error=sqrt(error*error*v5.av_val*v5.av_val+v5.error*v5.error*av_val*av_val)/(v5.av_val*v5.av_val);
         return qu2;
     }
 
-    void show(){ //вывод значения(окруление среднего значения и погрешности)
 
-        if(av_val>error){
+    bool operator==(const value& v6){
+        if(av_val==v6.av_val&&error==v6.error){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
 
-            if(error>1){
-                    error=round(error*10)/10;
-                    av_val=round(av_val*10)/10;
+};
+
+ void show(value& va){ //вывод значения(окруление среднего значения и погрешности)
+
+        if(va.av_val>va.error){
+            if(va.error>1){
+                    va.error=int(va.error*10)/10;
+                    va.av_val=round(va.av_val*10)/10;
             }
             else{
                 int n=0;
-                double f=error;
+                double f=va.error;
                 while(f<1){
                    f*=10;
                    n++;
                 }
-
-
-                if (int(error*pow(10,n+1)>=15)){
-
-                        error=round(error*pow(10,n))/pow(10,n);
-                        av_val=round(av_val*pow(10,n))/pow(10,n);
+                if(int(va.error*pow(10,n+1)>=15)){
+                        va.error=round(va.error*pow(10,n))/pow(10,n);
+                        va.av_val=round(va.av_val*pow(10,n))/pow(10,n);
                     }
-
                 else{
-                    error=round(error*pow(10,n+1))/pow(10,n+1);
-                    av_val=round(av_val*pow(10,n+1))/pow(10,n+1);
+                    va.error=round(va.error*pow(10,n+1))/pow(10,n+1);
+                    va.av_val=round(va.av_val*pow(10,n+1))/pow(10,n+1);
                     }
             }
-            cout<<av_val<<"+-"<<error<<endl;
+            cout<<"("<<va.av_val<<"+-"<<va.error<<")";
         }
-            else{
-                cout<<"maybe something is wrong"<<endl;
-            }
 
+
+    else{
+        if(va.av_val>1){
+            va.error=int(va.error*10)/10;
+            va.av_val=round(va.av_val*10)/10;
+            }
+        else{
+            int n=0;
+            double f=va.av_val;
+            while(f<1){
+                f*=10;
+                n++;
+            }
+        if (int(va.av_val*pow(10,n+1)>=15)){
+            va.error=round(va.error*pow(10,n))/pow(10,n);
+            va.av_val=round(va.av_val*pow(10,n))/pow(10,n);
+            }
+        else{
+            va.error=round(va.error*pow(10,n+1))/pow(10,n+1);
+            va.av_val=round(va.av_val*pow(10,n+1))/pow(10,n+1);
+            }
+        }
+        cout<<va.av_val<<"+-"<<va.error;
     }
 
-};
+}
 
     value pow(const value& v5, double n){
             value p;
@@ -194,23 +212,10 @@ struct value{
             return p;
         }
 
-
-
 int main(){
-    string name1, name2, name3;
-    cout<<"enter the name of the first variable: ";
-    getline(cin, name1);
-    cout<<"enter the name of the second variable: ";
-    getline(cin, name2);
-    cout<<"enter the name of the third variable: ";
-    getline(cin, name3);
-
-    value l(name1), w(name2), h(name3), V;
-
-    cout<<endl;
-
-    V=l*w*h;
-    cout<<"V=";
-    V.show();
+    value t("time"), s("distance"), u;
+    u=s/t;
+    cout<<"u=";
+    show(u);
     return 0;
 }
