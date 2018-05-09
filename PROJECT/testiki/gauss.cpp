b8a95dff07c4833e78d89fecb04e4391d7357e15
plplplrrr <32988112+LyudmilaPetrashko@users.noreply.github.com>
2018-05-09 13:13:21 +0300

    Create gauss.cpp

---
+++ PROJECT/testiki/gauss.cpp
@@ -0,0 +1,49 @@
+ввод                    вывод                       проверка
+
+1)matrix :              determinant=0               верно
+  0 0 0 
+  1 2 5 
+  4 3 2 
+
+2)matrix:               determinant=0               верно
+  0 1 2 
+  0 6 5 
+  0 4 3 
+  
+3)matrix:                determinant=0              верно
+  1 2 3 
+  1 2 3 
+  5 -7 8
+  
+4)matrix:                determinant=0                верно
+  1 1 5 
+  2 2 7 
+  3 3 -121   
+  
+5)matrix:                determinant=-9               верно
+  0 9 0 
+  1 2 0 
+  0 2 1 
+
+6)matrix:                 determinant=250             верно 
+  1 6 0 
+  8 1 9 
+  7 9 1 
+  
+7)matrix:                 determinant=0               верно 
+  0 9 0 1 
+  2 0 0 2 
+  3 0 0 5 
+  2 0 0 1   
+  
+8)matrix:                  determinant=0              верно 
+  2 0 0 7 
+  2 0 0 1 
+  1 2 1 2 
+  2 0 0 1   
+  
+9)matrix:                   determinant=-113/10         верно
+  0.7 2.4 
+  5 1   
+  
+10)  
