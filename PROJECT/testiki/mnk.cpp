16fcf0c40c8d060bfe883999c66ac322938728a4
plplplrrr <32988112+LyudmilaPetrashko@users.noreply.github.com>
2018-05-13 21:00:06 +0300

    Update mnk.cpp

--- PROJECT/testiki/mnk.cpp
+++ PROJECT/testiki/mnk.cpp
@@ -1,1 +1,46 @@
+1) Ввод
 
+enter 1 if you work with n degree polynom
+      2 if you work with dependence of some variables: 1
+
+enter polynom degree: 1
+
+enter the number of experiments: 3
+
+enter the value of X in the experiment number 1: 0.01
+enter the value of X in the experiment number 2: 0.02
+enter the value of X in the experiment number 3: 0.03
+
+enter the value of Y in the experiment number 1: 0.26
+enter the value of Y in the experiment number 2: 0.43
+enter the value of Y in the experiment number 3: 0.71
+
+  
+Вывод                                                                   Проверка
+
+f(x)=22.5*x+0.017                                                       верно
+
+R^2=0.9801                                                              верно
+
+
+2)Ввод
+
+enter polynom degree: 1
+
+enter the number of experiments: 4
+
+enter the value of X in the experiment number 1: 13
+enter the value of X in the experiment number 2: 14
+enter the value of X in the experiment number 3: 15
+enter the value of X in the experiment number 4: 16
+
+enter the value of Y in the experiment number 1: 13
+enter the value of Y in the experiment number 2: 14.5
+enter the value of Y in the experiment number 3: 16
+enter the value of Y in the experiment number 4: 15.9
+  
+Вывод                                                                    Проверка
+
+f(x)=1.02*x+0.06                                                          ???//кажется, верно
+
+R^2=0.8524                                                                ???
